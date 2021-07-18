4c2c612ef8e9b4865023b005465517e7fdaf844a
ahirner <a.hirner@gmail.com>
2021-07-18 20:55:45 +0200

    n_queens uses binary version per default

--- examples/n_queens.rs
+++ examples/n_queens.rs
@@ -1,21 +1,51 @@
 use std::error::Error;
 
-use backtrack::problems::NQueens;
-use backtrack::solvers::IterSolveNaive;
+use backtrack::problems::{BinQueens, NQueens};
+use backtrack::solvers::{IterSolveCached, IterSolveNaive};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
-    let n = match args.len() {
-        2 => args[1].parse()?,
-        _ => return Err(format!("Use: {} <n>", &args[0]).into()),
+
+    let usage = Err(format!(
+        "Use: {} <n> <bin|int> (defaults to `bin` up to usize width)",
+        &args[0]
+    )
+    .into());
+
+    let (n, bin) = match args.len() {
+        2 => {
+            let n = args[1].parse()?;
+            let bin = n <= BinQueens::max_n();
+            (n, bin)
+        }
+        3 => {
+            let n = args[1].parse()?;
+            let bin = match &args[2][..] {
+                "bin" => true,
+                "int" => false,
+                _ => return usage,
+            };
+            (n, bin)
+        }
+        _ => return usage,
     };
 
-    let n_queens = NQueens::new(n);
-    eprintln!("n_queens.rs: {:?}", n_queens);
+    if !bin {
+        let n_queens = NQueens::new(n);
+        eprintln!("n_queens.rs: {:?}", n_queens);
 
-    let solver = IterSolveNaive::new(&n_queens);
-    for solution in solver.into_iter() {
-        println!("{:?}", solution);
+        let solver = IterSolveNaive::new(&n_queens);
+        for solution in solver.into_iter() {
+            println!("{:?}", solution);
+        }
+    } else {
+        let bin_queens = BinQueens::new(n);
+        eprintln!("bin_queens.rs: {:?}", bin_queens);
+
+        let solver = IterSolveCached::new(&bin_queens);
+        for solution in solver.into_iter() {
+            println!("{:?}", solution);
+        }
     }
     Ok(())
 }
