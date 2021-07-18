b31df16ab430a158186bee39e912f9d085d29876
ahirner <a.hirner@gmail.com>
2021-07-18 15:50:15 +0200

    remove clone and lower other other bounds for T sat_safe

--- src/problems/utils.rs
+++ src/problems/utils.rs
@@ -4,19 +4,19 @@
 use crate::problem::{CheckInc, Scope};
 use std::fmt::Debug;
 
-fn sat<'a, T, P>(problem: &P, solution: Vec<T>) -> bool
+fn sat<'a, T, P, S>(problem: &P, solution: S) -> bool
 where
-    T: 'a + Clone,
+    T: 'a,
     P: Scope<'a, T> + CheckInc<T>,
-    P::Accumulator: Clone,
+    S: IntoIterator<Item = T>,
 {
-    let mut acc0 = None;
-    for (i, x_l) in solution.iter().enumerate() {
-        let acc = problem.fold_acc(acc0.clone(), &x_l, i);
-        if !problem.accu_sat(&acc, &x_l, i) {
+    let mut acc = None;
+    for (i, x_l) in solution.into_iter().enumerate() {
+        let acc1 = problem.fold_acc(acc, &x_l, i);
+        if !problem.accu_sat(&acc1, &x_l, i) {
             return false;
         }
-        acc0 = Some(acc);
+        acc = Some(acc1);
     }
     true
 }
@@ -25,18 +25,21 @@ where
 /// domain, then return `sat`
 pub(crate) fn sat_safe<'a, T, P, S>(problem: &'a P, solution: S) -> bool
 where
-    T: 'a + Clone + Eq + Debug,
+    T: 'a + PartialEq + Debug,
     P: Scope<'a, T> + CheckInc<T>,
-    P::Accumulator: Clone,
     S: IntoIterator<Item = T>,
     S::Item: std::fmt::Debug,
 {
-    let all: Vec<T> = solution.into_iter().collect();
-    assert!(all.len() <= problem.size(), "number of solutions cannot be beyond n");
+    let solution_all: Vec<T> = solution.into_iter().collect();
+    assert!(solution_all.len() <= problem.size(), "number of solutions cannot be beyond n");
     let domain: Vec<_> = problem.iter_values().collect();
-    for candidate in all.iter() {
-        let in_ = domain.contains(candidate);
-        assert!(in_, "candidate '{:?}' not part of domain {:?}", candidate, domain);
+    for candidate in solution_all.iter() {
+        assert!(
+            domain.contains(candidate),
+            "candidate '{:?}' not part of domain {:?}",
+            candidate,
+            domain
+        );
     }
-    sat(problem, all)
+    sat(problem, solution_all)
 }
